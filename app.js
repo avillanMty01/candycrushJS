de3fac8fd237c59444aa923c9b35883430492406
avillanMty01 <avillanMty01@yahoo.com>
2020-11-13 13:39:19 -0600

    added feature
    invalid moves when no match as suggested by @mizanxali

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const squares = []
   let score = 0
   const scoreDisplay = document.getElementById('scoreTag')
+  const scorePlusDisplay = document.getElementById('scorePlus')
   const candyColors = [
     'url(alt-images/alt-red.png)',
     'url(alt-images/alt-yellow.png)',
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const square = document.createElement('div')
       square.setAttribute('draggable', true) // drag square
       square.setAttribute('id', i) // set id for each square
-      let randomColor = Math.floor(Math.random() * candyColors.length)
+      const randomColor = Math.floor(Math.random() * candyColors.length)
       square.style.backgroundImage = candyColors[randomColor]
       grid.appendChild(square)
       squares.push(square) // elements into array of 64 squares
@@ -53,19 +54,14 @@ document.addEventListener('DOMContentLoaded', () => {
   function dragStart () {
     squareIdBeingDragged = parseInt(this.id) // store the id and color of the original square
     colorBeingDragged = this.style.backgroundImage
-    // console.log(colorBeingDragged)
-    // console.log(this.id, 'dragstart')
   }
   function dragOver (e) {
     e.preventDefault() // don't do anything
-    // console.log(this.id, 'dragover')
   }
   function dragEnter (e) {
     e.preventDefault() // don't do anything
-  // console.log(this.id, 'dragenter')
   }
   function dragLeave () {
-    // console.log(this.id, 'dragleave')
   }
   function dragDrop () {
     // console.log(this.id, 'dragdrop')
@@ -74,18 +70,27 @@ document.addEventListener('DOMContentLoaded', () => {
     this.style.backgroundImage = colorBeingDragged // change the color
     squares[squareIdBeingDragged].style.backgroundImage = colorBeingReplaced
   }
+
   function dragEnd () {
-    // console.log(this.id, 'dragend')
-    // what is a valid move ? we can't just drop anywhere our candy
-    let validMoves = [
-      squareIdBeingDragged - 1, // left
-      squareIdBeingDragged - width, // up
-      squareIdBeingDragged + 1, // right
-      squareIdBeingDragged + width // down
-    ]
-    let validMove = validMoves.includes(squareIdBeingReplaced)
+    // What is a valid move?
+    const validMoves = [squareIdBeingDragged - 1, squareIdBeingDragged - width, squareIdBeingDragged + 1, squareIdBeingDragged + width]
+    const validMove = validMoves.includes(squareIdBeingReplaced)
+
     if (squareIdBeingReplaced && validMove) {
-      squareIdBeingReplaced = null
+      const scoredRowOfFive = checkRowForFive()
+      const scoredColumnOfFive = checkColumnForFive()
+      const scoredRowOfFour = checkRowForFour()
+      const scoredColumnOfFour = checkColumnForFour()
+      const scoredRowOfThree = checkRowForThree()
+      const scoredColumnOfThree = checkColumnForThree()
+      // check if any combo was scored
+      // if no combo scored swap back to original candies
+      if (scoredRowOfFive || scoredColumnOfFive || scoredRowOfFour || scoredColumnOfFour || scoredRowOfThree || scoredColumnOfThree) {
+        squareIdBeingReplaced = null
+      } else if (!scoredRowOfFive && !scoredColumnOfFive && !scoredRowOfFour && !scoredColumnOfFour && !scoredRowOfThree && !scoredColumnOfThree) {
+        squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged
+        squares[squareIdBeingReplaced].style.backgroundImage = colorBeingReplaced
+      }
     } else if (squareIdBeingReplaced && !validMove) {
       squares[squareIdBeingReplaced].style.backgroundImage = colorBeingReplaced
       squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged
@@ -96,104 +101,128 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // --- Row of five
   function checkRowForFive () {
+    let scored = false
     for (let i = 0; i < 61; i++) {
-      let rowOfFive = [i, i + 1, i + 2, i + 3, i + 4]
-      let decideColor = squares[i].style.backgroundImage
+      const rowOfFive = [i, i + 1, i + 2, i + 3, i + 4]
+      const decideColor = squares[i].style.backgroundImage
       const isBlank = squares[i].style.backgroundImage === ''
       const notValid = [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38, 39, 44, 45, 46, 47, 52, 53, 54, 55]
       if (notValid.includes(i)) continue // avoids blank squares at edge of grid
       if (rowOfFive.every(index => squares[index].style.backgroundImage === decideColor && !isBlank)) {
         score += 5
+        scored = true
+        scorePlusDisplay.innerHTML = '+5'
         scoreDisplay.innerHTML = score
         rowOfFive.forEach(index => {
           squares[index].style.backgroundImage = ''
         })
       }
     }
+    return scored
   }
 
   // --- Column of five
   function checkColumnForFive () {
+    let scored = false
     for (let i = 0; i < 32; i++) { // originally Ania had i<47, but it didn't work dor the last column at botom
-      let columnOfFive = [i, i + width, i + width * 2, i + width * 3, i + width * 4]
-      let decideColor = squares[i].style.backgroundImage
+      const columnOfFive = [i, i + width, i + width * 2, i + width * 3, i + width * 4]
+      const decideColor = squares[i].style.backgroundImage
       const isBlank = squares[i].style.backgroundImage === ''
       if (columnOfFive.every(index => squares[index].style.backgroundImage === decideColor && !isBlank)) {
         score += 5
+        scored = true
         scoreDisplay.innerHTML = score
+        scorePlusDisplay.innerHTML = '+5'
         columnOfFive.forEach(index => {
           squares[index].style.backgroundImage = ''
         })
       }
     }
+    return scored
   }
 
   // --- Row of four
   function checkRowForFour () {
+    let scored = false
     for (let i = 0; i < 61; i++) {
-      let rowOfFour = [i, i + 1, i + 2, i + 3]
-      let decideColor = squares[i].style.backgroundImage
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const decideColor = squares[i].style.backgroundImage
       const isBlank = squares[i].style.backgroundImage === ''
       const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
       if (notValid.includes(i)) continue // avoids blank squares at edge of grid
       if (rowOfFour.every(index => squares[index].style.backgroundImage === decideColor && !isBlank)) {
         score += 4
+        scored = true
         scoreDisplay.innerHTML = score
+        scorePlusDisplay.innerHTML = '+4'
         rowOfFour.forEach(index => {
           squares[index].style.backgroundImage = ''
         })
       }
     }
+    return scored
   }
 
   // --- Column of four
   function checkColumnForFour () {
+    let scored = false
     for (let i = 0; i < 40; i++) { // originally Ania had i<47, but it didn't work dor the last column at botom
-      let columnOfFour = [i, i + width, i + width * 2, i + width * 3]
-      let decideColor = squares[i].style.backgroundImage
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+      const decideColor = squares[i].style.backgroundImage
       const isBlank = squares[i].style.backgroundImage === ''
       if (columnOfFour.every(index => squares[index].style.backgroundImage === decideColor && !isBlank)) {
         score += 4
+        scored = true
         scoreDisplay.innerHTML = score
+        scorePlusDisplay.innerHTML = '+4'
         columnOfFour.forEach(index => {
           squares[index].style.backgroundImage = ''
         })
       }
     }
+    return scored
   }
 
   // --- Row of three
   function checkRowForThree () {
+    let scored = false
     for (let i = 0; i < 62; i++) {
-      let rowOfThree = [i, i + 1, i + 2]
-      let decideColor = squares[i].style.backgroundImage
+      const rowOfThree = [i, i + 1, i + 2]
+      const decideColor = squares[i].style.backgroundImage
       const isBlank = squares[i].style.backgroundImage === ''
       const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
       if (notValid.includes(i)) continue // avoids blank squares at edge of grid
       if (rowOfThree.every(index => squares[index].style.backgroundImage === decideColor && !isBlank)) {
         score += 3
+        scored = true
         scoreDisplay.innerHTML = score
+        scorePlusDisplay.innerHTML = '+3'
         rowOfThree.forEach(index => {
           squares[index].style.backgroundImage = ''
         })
       }
     }
+    return scored
   }
 
   // --- Column of three
   function checkColumnForThree () {
+    let scored = false
     for (let i = 0; i < 48; i++) { // originally Ania had i<47, but it didn't work dor the last column at botom
-      let columnOfThree = [i, i + width, i + width * 2]
-      let decideColor = squares[i].style.backgroundImage
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decideColor = squares[i].style.backgroundImage
       const isBlank = squares[i].style.backgroundImage === ''
       if (columnOfThree.every(index => squares[index].style.backgroundImage === decideColor && !isBlank)) {
         score += 3
+        scored = true
         scoreDisplay.innerHTML = score
+        scorePlusDisplay.innerHTML = '+3'
         columnOfThree.forEach(index => {
           squares[index].style.backgroundImage = ''
         })
       }
     }
+    return scored
   }
 
   function moveDown () {
@@ -206,12 +235,13 @@ document.addEventListener('DOMContentLoaded', () => {
       const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
       const isFirstRow = firstRow.includes(i)
       if (isFirstRow && (squares[i].style.backgroundImage === '')) {
-        let randomColor = Math.floor(Math.random() * candyColors.length)
+        const randomColor = Math.floor(Math.random() * candyColors.length)
         squares[i].style.backgroundImage = candyColors[randomColor]
       }
     }
   }
-  // every 100 miliseconds check if theres a row match of three
+
+  // every 100 miliseconds check if there's a row match of three
   window.setInterval(function () {
     moveDown()
     checkRowForFive()
